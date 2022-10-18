0db24dfb0b93e43ddaa77186bb3fe9ba90673bbb
amisha2002 <64023444+amisha2002@users.noreply.github.com>
2022-10-18 19:35:02 +0530

    Delete spring-boot-crud-employee directory

--- spring-boot-crud-employee/src/main/java/com/example/springbootcrudemployee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootcrudemployee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springbootcrudemployee.dto.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
-
-}
